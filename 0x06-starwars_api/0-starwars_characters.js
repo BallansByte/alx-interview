f025cf3b12275a9e671670f909e7c3904db6e7d7
Ballans <maballans@gmail.com>
2024-09-06 08:33:56 -0700

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -10,26 +10,20 @@ const request = require('request');
 
 // Recursive function to get and print character names from a list of URLs
 function printNextCharacter (urls) {
-  // Remove first URL from list and parse it
   const characterURL = urls.shift();
   if (characterURL) {
     request(characterURL, function (error, response, body) {
       if (!error) {
-        // Parse and print the character name from the response body
         console.log(JSON.parse(body).name);
-        // Move to next character in the list
         printNextCharacter(urls);
       }
     });
   }
 }
 
-// Fetches a list of character's URLs then uses a callback to print each one's name
 function printStarWarsCharacters (url) {
-  // Get list of URLs for characters in selected film
   request(url, function (error, response, body) {
     if (!error) {
-      // Call recursive function to fetch and print character names from URLs list
       printNextCharacter(JSON.parse(body).characters);
     }
   });
